897dc899d0f7639cbb10ecf06870f6af24d3a530
mashkust <mashkusti@gmail.com>
2021-09-27 13:29:15 +0300

    Корретировка вспомогательных функций

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 function getRandomInt (min,max) {
   min=Math.ceil(min);
   max=Math.floor(max);
-  return Math.floor(Math.random()* (max - min) + min);
+  return Math.floor(Math.random()* (max - min +1)) + min;
 }
 
 function getPositiveInt (min,max) {
+  if (typeof Number(min)==='number' && typeof Number(max)==='number') {
+    min=Number(min);
+    max=Number(max);
+  }
   if (typeof min==='number' && typeof max==='number' &&  min>=0 && max>=0) {
     if (min>max) {
-      const NUM=min;
+      const NUM1=min;
       min=max;
-      max=NUM;
+      max=NUM1;
       return getRandomInt(min,max);
     } else if (min<max || (min===max && Number.isInteger(min)) ) {
       return getRandomInt (min,max);
@@ -28,11 +32,16 @@ function getRandomFract (min, max, decimal) {
 }
 
 function getPositiveFract (min,max, decimal) {
-  if (typeof min==='number' && typeof max==='number' && typeof decimal==='number' && min>=0 && max>=0 && decimal>=0 && Number.isInteger(decimal)) {
+  if (typeof Number(min)==='number' && typeof Number(max)==='number' && typeof Number(decimal)==='number') {
+    min=Number(min);
+    max=Number(max);
+    decimal=Number(decimal);
+  }
+  if (typeof min==='number' && typeof max==='number' && min>=0 && max>=0 && decimal>=0 && Number.isInteger(decimal)) {
     if (min>max) {
-      const NUM=min;
+      const NUM2=min;
       min=max;
-      max=NUM;
+      max=NUM2;
       return getRandomFract (min, max, decimal);
     } else if (min<max || min===max ) {
       return getRandomFract (min, max, decimal);
