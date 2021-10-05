abe0d85ac419e6cb2c043c65f2fd6ce1b380c617
mashkust <mashkusti@gmail.com>
2021-10-05 16:55:31 +0300

    больше деталей с вопросами

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const getPositiveInt = (min, max) => {
     } return null;
   } return null;
 };
-getPositiveInt(5,10);
 
 const getRandomFloatRange = (min, max, decimal) => {
   decimal = 10 ** decimal;
@@ -44,5 +43,95 @@ const getPositiveFloatRange = (min, max, decimal) => {
     }
   } return null;
 };
-getPositiveFloatRange(2.5,10.8,3);
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECK = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const LENGHT_ADS_ARRAY = 10 ;
+
+const getArray = (some, min, max) => {
+  const MAX_LENGHT = some.length;
+  const LENGHT_ARRAY = getPositiveInt(1, MAX_LENGHT);
+  const ARRAY = [];
+  for(let index = 0; index < LENGHT_ARRAY; index++) {
+    const INDEX_EL = getPositiveInt(min, max);
+    const ELEM = some[INDEX_EL];
+    if (some === FEATURES && !ARRAY.includes(ELEM)) {
+      ARRAY.push(ELEM);
+    }
+    if(some === PHOTOS) {
+      ARRAY.push(ELEM);
+    }
+  }
+  return ARRAY;
+};
+
+const createAuthor = (xx) => ({
+  avatar:`img/avatars/user${xx}.png`,
+});
+
+const createLocation = () => ({
+  lat: getPositiveFloatRange(35.65, 35.7, 5),
+  lng: getPositiveFloatRange(139.7, 139.8, 5),
+});
+
+const createOffer = (lat, lng) => ({
+  title: 'Сдаётся',
+  address: `${lat},${lng}`,// как связать с location стрелочные функции
+  price: getPositiveInt(0,1000), //любое
+  type: TYPES[getPositiveInt(0,4)],
+  rooms: getPositiveInt(0,10), //любое
+  guests: getPositiveInt(0,10), //любое
+  checkin: CHECK[getPositiveInt(0,2)],
+  checkout: CHECK[getPositiveInt(0,2)],
+  features: getArray(FEATURES, 0, 5),
+  description: 'Квартира',
+  photos: getArray(PHOTOS, 0, 2),
+});
+
+const createAd = (xx) => {
+  const XX=xx;
+  return{
+    author: createAuthor(XX),
+    location: createLocation(), // ?
+    offer: createOffer(createLocation().lat,createLocation().lng), // разное местоположение
+  };};
+
+const createAds = () => {
+  const ADS_ARRAY = [];
+  for(let index = 0; index < LENGHT_ADS_ARRAY-1; index++) {
+    ADS_ARRAY[index] = createAd(`0${index+1}`);
+  }
+  ADS_ARRAY[LENGHT_ADS_ARRAY-1] = createAd(LENGHT_ADS_ARRAY);
+  return ADS_ARRAY;
+};
+
+const ADS = createAds();
+
 
